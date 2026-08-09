c46eb5883a4b0195c30bf32813d0e49008bf00a3
Sambhav Pandey <sambhavpandey1234@gmail.com>
2026-08-09 23:48:20 +0530

    Add files via upload

---
+++ assignment12q2.java
@@ -0,0 +1,9 @@
+import library.Book;
+
+public class assignment12q2 {
+    public static void main(String[] args) {
+
+        Book b = new Book();
+        b.display();
+    }
+}
---
+++ book.java
@@ -0,0 +1,10 @@
+package library;
+
+public class Book {
+    public void display() {
+        System.out.println("Book ID: 101");
+        System.out.println("Title: Java Programming");
+        System.out.println("Author: James Gosling");
+        System.out.println("Price: ₹599");
+    }
+}
